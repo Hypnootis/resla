0d81e3a1f281c3203008765361be8ac71346f0ce
Hypnootis <tm.pasanen@outlook.com>
2022-04-06 18:41:41 +0300

    New control code, nonworking so far

--- esp32/compile.sh
+++ esp32/compile.sh
@@ -1,2 +1,2 @@
-arduino-cli compile --fqbn esp32:esp32:esp32 tommi-control-code
-arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32 tommi-control-code
+arduino-cli compile --fqbn esp32:esp32:esp32 resla-controls
+arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32 resla-controls
---
+++ esp32/resla-controls/resla-controls.ino
@@ -0,0 +1,35 @@
+#include "ESP32Servo.h"
+
+Servo servo_motor;
+Servo esc_motor;
+
+/*
+	d = left
+	s = straight
+	l = right
+*/
+
+void setup() {
+	Serial.begin(115200);
+	servo_motor.attach(4);
+	esc_motor.attach(5);
+	Serial.println("Serial connection established");
+}
+
+void loop() {
+	if (Serial.available()){
+	int value = Serial.read();
+	Serial.write(value);
+
+	if (value == 115){
+		servo_motor.write(90);
+		}
+
+	if (value == 108){
+		servo_motor.write(30);
+		}
+	if (value == 100){
+		servo_motor.write(130);
+		}
+	}
+}
