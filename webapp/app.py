a463f31e6d714d92293410a883da285434b159e4
Hypnootis <tm.pasanen@outlook.com>
2022-04-07 13:20:20 +0300

    Website running

--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, request
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 
 servo_pin = 19
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
 
-#p = GPIO.PWM(servo_pin, 50)
+p = GPIO.PWM(servo_pin, 50)
 
-#p.start(0)
+p.start(0)
 
 app = Flask(__name__, template_folder="static")
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/control_servo", methods=["POST"])
+def control_servo():
+    slider = request.form["slider"]
+    p.ChangeDutyCycle(float(slider))
+    sleep(1)
+    p.ChangeDutyCycle(0)
     return render_template("index.html")
 
 if __name__ == "__main__":
