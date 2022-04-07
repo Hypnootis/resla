691fce5d395713e85359e46c97058fdff1785630
hypnootis <tm.pasanen@outlook.com>
2022-04-07 13:00:13 +0300

    Made slider

--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, request
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from time import sleep
 
 servo_pin = 19
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
 
-p = GPIO.PWM(servo_pin, 50)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(servo_pin, GPIO.OUT)
 
-p.start(0)
+#p = GPIO.PWM(servo_pin, 50)
 
-app = Flask(__name__)
+#p.start(0)
+
+app = Flask(__name__, template_folder="static")
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
