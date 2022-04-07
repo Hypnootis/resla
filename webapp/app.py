9dca0ec831857ac6e7e9462e88984df0a2ff605a
Hypnootis <tm.pasanen@outlook.com>
2022-04-07 12:49:04 +0300

    Ditched esp32

---
+++ webapp/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+from time import sleep
+
+servo_pin = 19
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+p = GPIO.PWM(servo_pin, 50)
+
+p.start(0)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
