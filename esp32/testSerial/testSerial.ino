6f5a99bc3a60643589f52c36e617790f01468ce4
Hypnootis <tm.pasanen@outlook.com>
2022-04-13 13:21:58 +0300

    wtf serial actually works

--- esp32/testSerial/testSerial.ino
+++ esp32/testSerial/testSerial.ino
@@ -1,18 +1,40 @@
+#include <ESP32Servo.h>
+
 String nom = "ESP32";
 String msg;
+Servo steering;
 
 
 void setup() {
 	Serial.begin(115200);
+	steering.attach(4);
 }
 
 void loop() {
 	readSerialPort();
 
 	if (msg != ""){
+		if (msg == "left"){
+			steering.write(130);
+		}
+		else if (msg == "right"){
+		
+			steering.write(30);
+		}
+		else if (msg == "straight"){
+			steering.write(90);
+		}
 	sendData();
 	}
-	delay(500);
+	/*
+	if (Serial.available()){
+		delay(10);
+		while (Serial.available() > 0){
+			
+	}
+	}
+*/
+	 delay(500);
 }
 
 void readSerialPort(){
