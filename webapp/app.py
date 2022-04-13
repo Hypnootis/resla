d8d302d21d264c5ca7ac29744302a94bfd21a5b2
Nex12315 <nikita20021804@gmail.com>
2022-04-13 13:44:15 +0300

    cnganged backend code

--- webapp/app.py
+++ webapp/app.py
@@ -1,52 +1,35 @@
-from flask import Flask, render_template, request
-import RPi.GPIO as GPIO
+from flask import Flask, render_template
+import serial
 import time
 
-servoPin = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPin, GPIO.OUT)
+print("Running")
 
-p = GPIO.PWM(servoPin, 50)
-
-
-def turn_right():
-    p.start(0)
-    try:
-        p.ChangeDutyCycle(2.5)
-        print("Turning wheels right")
-        time.sleep(0.5)
-    except:
-        print("Whoops, error with the controls")
-
-
-def turn_left():
-    p.start(0)
-    try:
-        p.ChangeDutyCycle(10)
-        print("Turning wheels left")
-        time.sleep(0.5)
-    except:
-        print("Whoops, error with the controls")
-
-
-def reset_wheels():
-    p.start(0)
-    try:
-        p.ChangeDutyCycle(7.5)
-        print("Straightening wheels")
-        time.sleep(1.5)
-    except:
-        print("Whoops, error with the controls")
+# with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as esp:
+#     time.sleep(0.1)
+#     if esp.isOpen():
+#         print(f"{esp.port} connected!")
+#         try:
+#             while True:
+#                 cmd = input("Enter command:")
+#                 esp.write(cmd.encode())
+#                 while esp.inWaiting() == 0:
+#                     pass
+#                 if esp.inWaiting() > 0:
+#                     answer = esp.readline()
+#                     print(answer)
+#                     esp.flushInput()
+#         except KeyboardInterrupt:
+#             print("Exiting...")
 
 
 app = Flask(__name__, template_folder="templates")
-LEFT, RIGHT, FORWARD, BACKWARD, RESET, STOP = "left", "right", 'forward', 'backward', 'reset', "stop"
+LEFT, RIGHT, FORWARD, BACKWARD, STRAIGHT, STOP = "left", "right", 'forward', 'backward', 'straight', "stop"
 AVAILABLE_COMMANDS = {
     "Left": LEFT,
     "Right": RIGHT,
     "Forward": FORWARD,
     "Backward": BACKWARD,
-    "Reset": RESET,
+    "Straight": STRAIGHT,
     "Stop": STOP
 }
 
@@ -56,39 +39,57 @@ def execute():
     return render_template("main.html", commands=AVAILABLE_COMMANDS)
 
 
+@app.route('/<cmd>')
+def command(cmd=None):
+    # if cmd1 == STRAIGHT:
+    #     camera_command = cmd1
+    #     response = "Resetting ..."
+    # else:
+    #     # camera_command = cmd[0].upper()
+    #     camera_command = cmd1
+    #     response = "Moving {}".format(cmd1.capitalize())
+
+    with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as esp:
+        time.sleep(0.1)
+        if esp.isOpen():
+            print(f"{esp.port} connected!")
+            response = "Moving {}".format(cmd.capitalize())
+            try:
+                while True:
+                    servo_command = cmd
+                    if servo_command != "":
+                        esp.write(servo_command.encode())
+                        while esp.inWaiting() == 0:
+                            pass
+                        if esp.inWaiting() > 0:
+                            answer = esp.readline()
+                            print(answer)
+                            esp.flushInput()
+            except KeyboardInterrupt:
+                print("Exiting...")
+
+    print(servo_command)
+    return response, 200, {'Content-Type': 'text/plain'}
+
 # @app.route('/<cmd>')
 # def command(cmd=None):
-#     if cmd == RESET:
+#     if cmd == STRAIGHT:
 #         camera_command = "X"
 #         response = "Resetting ..."
 #     else:
-#         # camera_command = cmd[0].upper()
+#         camera_command = cmd[0].upper()
 #         camera_command = cmd
 #         response = "Moving {}".format(cmd.capitalize())
-#
-#     print(camera_command)
+#         if camera_command == STRAIGHT:
+#             reset_wheels()
+#             response = "Moving {}".format(cmd.capitalize())
+#         elif camera_command == LEFT:
+#             turn_left()
+#             response = "Moving {}".format(cmd.capitalize())
+#         elif camera_command == RIGHT:
+#             turn_right()
+#             response = "Moving {}".format(cmd.capitalize())
 #     return response, 200, {'Content-Type': 'text/plain'}
-
-@app.route('/<cmd>')
-def command(cmd=None):
-    if cmd == RESET:
-        camera_command = "X"
-        response = "Resetting ..."
-        reset_wheels()
-    else:
-        camera_command = cmd[0].upper()
-        camera_command = cmd
-        response = "Moving {}".format(cmd.capitalize())
-    #if camera_command == RESET:
-    #    reset_wheels()
-    #    response = "Moving {}".format(cmd.capitalize())
-    if camera_command == LEFT:
-        turn_left()
-        response = "Moving {}".format(cmd.capitalize())
-    elif camera_command == RIGHT:
-        turn_right()
-        response = "Moving {}".format(cmd.capitalize())
-    return response, 200, {'Content-Type': 'text/plain'}
 
 
 if __name__ == '__main__':
